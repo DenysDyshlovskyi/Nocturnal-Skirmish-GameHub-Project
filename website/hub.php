7da12ad699e7e28b1de2641372c6eea1f5a4669b
MagiscCow <ddyshlovskyi@gmail.com>
2024-10-14 11:22:39 +0200

    Imported Hub2 into hub.php in an effort to merge progress

--- website/hub.php
+++ website/hub.php
@@ -31,6 +31,76 @@ require "./php_scripts/get_loggedin_info.php";
             <button class="hub-corner-profile-dropdown-button" id="dropdown-button-chats" title="Chats"></button>
         </div>
     </div>
+
+
+<!-- HUB2-IMPORT -->
+    <!-- Middle Menu -->
+    <div
+        style="display: flex; justify-content: center; flex-direction: row; align-items: center; gap: 20px; margin-top: 10%;">
+        <div class="main-menu-middle-ui"
+            style="display: flex; justify-content: center; flex-direction: column; align-items: center; gap: 20px;">
+            <h1 class="title-text">GameHub™</h1>
+            <button class="play-button">Play <img style="width: 30%;" src="img/Noc_Skir_Logo.svg" alt="Logo"></button>
+            <div class="menu-selection-buttons">
+                <button style="margin-bottom: 10px; margin-right: 10px;" class="menu-button">Leaderboard</button>
+                <button class="menu-button"><a class="link" href="../website/shop/Featured.html">Shop</a></button>
+                <br>
+                <button style="margin-bottom: 10px; margin-right: 10px;" class="menu-button">Tutorial</button>
+                <button class="menu-button">Friends List</button>
+            </div>
+        </div>
+
+        <!-- Patch Notes UI container -->
+        <div class="patch-notes-box">
+            <h2 class="patch-notes-title">Patch Notes</h2>
+            <div class="patch-notes-content-grey">
+                <div class="patch-note-list">
+                    <div class="patch-note">
+                        <h3 class="patch-note-title">Version 1.0.0</h3>
+                        <ul class="patch-note-list-items">
+                            <li>Added new stuff</li>
+                            <li>Updated shop items</li>
+                            <li>Added leaderboard feature</li>
+                        </ul>
+                    </div>
+                    <div class="patch-note">
+                        <h3 class="patch-note-title">Version 0.9.8</h3>
+                        <ul class="patch-note-list-items">
+                            <li>Added new stuff</li>
+                            <li>Updated shop items</li>
+                            <li>Added new stuff</li>
+                        </ul>
+                    </div>
+                    <div class="patch-note">
+                        <h3 class="patch-note-title">Version 0.9.7</h3>
+                        <ul class="patch-note-list-items">
+                            <li>Added new stuff</li>
+                            <li>Updated shop items</li>
+                            <li>Added new stuff</li>
+                        </ul>
+                    </div>
+                    <div class="patch-note">
+                        <h3 class="patch-note-title">Version 0.9.6</h3>
+                        <ul class="patch-note-list-items">
+                            <li>Added new stuff</li>
+                            <li>Updated shop items</li>
+                            <li>Added new stuff</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>     
+
+        <!-- Footer container with trademark text and live player count -->
+        <footer>
+            <p class="footer-tm-text">GameHub™ 2024</p>
+            <p class="player-live-count-text">5 players online</p>
+            <img class="live-count-icon" src="img/icons/live-count.svg" alt="live count icon">
+        </footer>
+    <!-- /HUB2-IMPORT -->
+
+    <!-- Autolooping audio background music (works only if user allows it) -->
     <audio autoplay loop style="display: none;">
         <source src="audio/BrowsingShopOST.mp3" type="audio/mpeg">
     </audio>
