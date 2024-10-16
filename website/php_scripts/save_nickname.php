28bd52aef914b68b638d7fe89f1b373c0ebf3514
unknown <jakob.johannes.hansgaard@outlook.com>
2024-10-16 12:13:59 +0200

    Added error handling to nickname change

--- website/php_scripts/save_nickname.php
+++ website/php_scripts/save_nickname.php
@@ -2,10 +2,16 @@
 require "avoid_errors.php";
 //Saves new nickname to database
 if(isset($_POST['nickname'])){
-    $stmt = $conn->prepare("UPDATE users SET nickname = ? WHERE user_id = ?");
-    $stmt->bind_param("ss", $_POST['nickname'], $_SESSION['user_id']);
-    $stmt->execute();
-    $stmt->close();
-    echo $_POST['nickname'];
+    if ($_POST['nickname'] < 0) {
+        echo "error";
+    } else {
+        $stmt = $conn->prepare("UPDATE users SET nickname = ? WHERE user_id = ?");
+        $stmt->bind_param("ss", $_POST['nickname'], $_SESSION['user_id']);
+        $stmt->execute();
+        $stmt->close();
+        echo $_POST['nickname'];
+    }
+} else {
+    echo "error";
 };
 ?>
