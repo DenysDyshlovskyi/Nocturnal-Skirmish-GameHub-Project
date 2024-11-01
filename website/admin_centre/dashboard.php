c9cf70ec1a27ade5eb3209b489ffd6049c27a455
unknown <jakob.johannes.hansgaard@outlook.com>
2024-11-01 11:21:32 +0100

    minor spelling fix

--- website/admin_centre/dashboard.php
+++ website/admin_centre/dashboard.php
@@ -2,7 +2,7 @@
 // Dashboard for admins
 session_start();
 
-// If user is unautherized, redirect them
+// If user is unauthorized, redirect them
 if (!isset($_SESSION['isadmin']) || $_SESSION['isadmin'] != 1) {
     header("Location: admin_login.php?error=unauth");
 }
