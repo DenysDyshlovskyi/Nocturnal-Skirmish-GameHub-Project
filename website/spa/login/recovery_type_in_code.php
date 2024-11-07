e87ca1c96e2d6d050efff9cb97f02f1e3ae9dbbc
unknown <jakob.johannes.hansgaard@outlook.com>
2024-11-07 15:59:52 +0100

    minor tweaks

--- website/spa/login/recovery_type_in_code.php
+++ website/spa/login/recovery_type_in_code.php
@@ -8,6 +8,7 @@
 </style>
 <div class="forgot-link-container" id="type-in-code">
     <p>A code has been sent to <?php echo $_SESSION['temp_recovery_email']; ?>. Please type in the code within 5 minutes.</p>
+    <p><i>*If you cannot find the e-mail, check your spam folder.</i></p>
     <input type="text" id="recovery_code_input" placeholder="000000" maxlength="6">
     <div class="forgot-link-button-container">
         <button class="forgot-link-next-button" onclick="recoveryCode()">Next</button>
