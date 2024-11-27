8d669d3f8214775d35cb5dc3755563110d1384fb
unknown <jakob.johannes.hansgaard@outlook.com>
2024-11-27 09:41:12 +0100

    Patched border exploit

--- website/php_scripts/save_border.php
+++ website/php_scripts/save_border.php
@@ -4,6 +4,20 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     require "avoid_errors.php";
     $bordername = htmlspecialchars($_POST['bordername']);
     if (file_exists("../img/borders/" . $bordername)) {
+        // Check if border is in users inventory
+        $stmt = $conn->prepare("SELECT * FROM border_inventory WHERE user_id = ? AND border = ?");
+        $stmt->bind_param("ss", $_SESSION['user_id'], $bordername);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ((mysqli_num_rows($result) <= 0)) {
+            // If the user doesnt have the border in their inventory
+            echo "error";
+            $stmt->close();
+            exit;
+        }
+        $stmt->close();
+
+        // Set the border
         $stmt = $conn->prepare("UPDATE users SET profile_border = ? WHERE user_id = ?");
         $stmt->bind_param("ss", $bordername, $_SESSION['user_id']);
         $stmt->execute();
