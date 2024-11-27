17459828388ff9ad967c2d8fd7ad9b2ab9e14716
unknown <jakob.johannes.hansgaard@outlook.com>
2024-11-27 09:50:41 +0100

    extra security for groupchat settings

--- website/php_scripts/open_groupchat_settings.php
+++ website/php_scripts/open_groupchat_settings.php
@@ -1,32 +1,36 @@
 <?php
 // Prepares opening of groupchat settings
-require "avoid_errors.php";
-$groupchat = htmlspecialchars($_POST['groupchat']);
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require "avoid_errors.php";
+    $groupchat = htmlspecialchars($_POST['groupchat']);
 
-// Check if the current chat is a groupchat and if the user has access
-if (isset($_SESSION['current_table'])) {
-    // Does the user have access to the chat?
-    $stmt = $conn->prepare("SELECT * FROM chats WHERE tablename = ? AND user_id = ?");
-    $stmt->bind_param("ss", $groupchat, $_SESSION['user_id']);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if($result->num_rows === 0){
+    // Check if the current chat is a groupchat and if the user has access
+    if (isset($_SESSION['current_table'])) {
+        // Does the user have access to the chat?
+        $stmt = $conn->prepare("SELECT * FROM chats WHERE tablename = ? AND user_id = ?");
+        $stmt->bind_param("ss", $groupchat, $_SESSION['user_id']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result->num_rows === 0){
+            echo "error";
+            exit;
+        }
+        $stmt->close();
+
+        // Is the chat a groupchat?
+        $stmt = $conn->prepare("SELECT * FROM chats WHERE tablename = ? AND type = 'groupchat'");
+        $stmt->bind_param("s", $groupchat);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result->num_rows === 0){
+            echo "not_groupchat";
+            exit;
+        }
+        $stmt->close();
+    } else {
         echo "error";
         exit;
     }
-    $stmt->close();
-
-    // Is the chat a groupchat?
-    $stmt = $conn->prepare("SELECT * FROM chats WHERE tablename = ? AND type = 'groupchat'");
-    $stmt->bind_param("s", $groupchat);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if($result->num_rows === 0){
-        echo "not_groupchat";
-        exit;
-    }
-    $stmt->close();
 } else {
-    echo "error";
-    exit;
+    header("Location: ../index.php");
 }
--- website/spa/messages/groupchat_settings.php
+++ website/spa/messages/groupchat_settings.php
@@ -1,3 +1,35 @@
+<?php
+// Really make sure the user has access to the groupchat
+require "../../php_scripts/avoid_errors.php";
+
+// Check if the current chat is a groupchat and if the user has access
+if (isset($_SESSION['current_table'])) {
+    // Does the user have access to the chat?
+    $stmt = $conn->prepare("SELECT * FROM chats WHERE tablename = ? AND user_id = ?");
+    $stmt->bind_param("ss", $groupchat, $_SESSION['user_id']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($result->num_rows === 0){
+        echo "Error <button onclick='removeDarkContainer()'>Close</button>";
+        exit;
+    }
+    $stmt->close();
+
+    // Is the chat a groupchat?
+    $stmt = $conn->prepare("SELECT * FROM chats WHERE tablename = ? AND type = 'groupchat'");
+    $stmt->bind_param("s", $groupchat);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($result->num_rows === 0){
+        echo "Error <button onclick='removeDarkContainer()'>Close</button>";
+        exit;
+    }
+    $stmt->close();
+} else {
+    echo "Error <button onclick='removeDarkContainer()'>Close</button>";
+    exit;
+}
+?>
 <style><?php include "./css/groupchat-settings.css" ?></style>
 <div class="groupchat-settings-container">
 
