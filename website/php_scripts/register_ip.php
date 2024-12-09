3f458721bb6ed40aa0dc37310d13cd6408629355
unknown <jakob.johannes.hansgaard@outlook.com>
2024-12-09 23:44:20 +0100

    added an array of ip adresses that should be ignored

--- website/php_scripts/register_ip.php
+++ website/php_scripts/register_ip.php
@@ -5,28 +5,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     require "get_user_ip.php";
 
-    // Check if ip has already been registered
-    $stmt = $conn->prepare("SELECT * FROM ip_adresses WHERE user_id = ? AND ip = ?");
-    $stmt->bind_param("ss", $_SESSION['user_id'], $ip);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    // Get current date and time
-    require "getdate.php";
-    $last_login = $date . " " . $time;
-
-    if($result->num_rows === 0){
-        // If it hasnt been registered
-        $stmt = $conn->prepare("INSERT INTO ip_adresses (user_id, ip, last_login) VALUES (?,?,?)");
-        $stmt->bind_param("sss", $_SESSION['user_id'], $ip, $last_login);
+    // Blacklist of ips to be ignored. If the ip is in blacklist, dont register it
+    $ignore = array("193.156.164.219", "127.0.0.1", "::1");
+    if (!in_array($ip, $ignore)) {
+        // Check if ip has already been registered
+        $stmt = $conn->prepare("SELECT * FROM ip_adresses WHERE user_id = ? AND ip = ?");
+        $stmt->bind_param("ss", $_SESSION['user_id'], $ip);
         $stmt->execute();
-        $stmt->close();
-    } else {
-        //If it has been registered
-        $stmt = $conn->prepare("UPDATE ip_adresses SET last_login = ? WHERE user_id = ? AND ip = ?");
-        $stmt->bind_param("sss", $last_login, $_SESSION['user_id'], $ip);
-        $stmt->execute();
-        $stmt->close();
+        $result = $stmt->get_result();
+
+        // Get current date and time
+        require "getdate.php";
+        $last_login = $date . " " . $time;
+
+        if($result->num_rows === 0){
+            // If it hasnt been registered
+            $stmt = $conn->prepare("INSERT INTO ip_adresses (user_id, ip, last_login) VALUES (?,?,?)");
+            $stmt->bind_param("sss", $_SESSION['user_id'], $ip, $last_login);
+            $stmt->execute();
+            $stmt->close();
+        } else {
+            //If it has been registered
+            $stmt = $conn->prepare("UPDATE ip_adresses SET last_login = ? WHERE user_id = ? AND ip = ?");
+            $stmt->bind_param("sss", $last_login, $_SESSION['user_id'], $ip);
+            $stmt->execute();
+            $stmt->close();
+        }
     }
 } else {
     header("Location: ../index.php");
