d213e2e578c257d247f3bfd7951a451f99a20111
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2024-12-12 15:22:56 +0100

    gamemode selections

--- website/spa/nocturnal-skirmish/gamemode_selection.php
+++ website/spa/nocturnal-skirmish/gamemode_selection.php
@@ -8,10 +8,50 @@
     </div>
     <div class="flickity-container">
         <div class="main-carousel">
-            <div class="carousel-cell"><img src="./img/cards/Water_Spirit_Card.webp" alt=""></div>
-            <div class="carousel-cell">jakob</div>
-            <div class="carousel-cell">denys</div>
-            <div class="carousel-cell">zackify</div>
+            <div class="carousel-cell">
+                <div class="cell-header">
+                    Casual
+                </div>
+                <div class="carousel-pixelart-container" style="background-image: url(./img/cards/CryoEruption_Card.webp);"></div>
+                <div class="carousel-select-button-container-container">
+                    <div class="carousel-select-button-container">
+                        <button title="Select casual gamemode">Select</button>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="cell-header">
+                    Intermediate
+                </div>
+                <div class="carousel-pixelart-container" style="background-image: url(./img/cards/Eruption_Card.webp);"></div>
+                <div class="carousel-select-button-container-container">
+                    <div class="carousel-select-button-container">
+                        <button title="Select intermediate gamemode">Select</button>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="cell-header">
+                    Ranked
+                </div>
+                <div class="carousel-pixelart-container" style="background-image: url(./img/cards/FireballVolley_Card.webp);"></div>
+                <div class="carousel-select-button-container-container">
+                    <div class="carousel-select-button-container">
+                        <button title="Select ranked gamemode">Select</button>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="cell-header">
+                    Dual
+                </div>
+                <div class="carousel-pixelart-container" style="background-image: url(./img/cards/DivergentSpirit_Card.webp);"></div>
+                <div class="carousel-select-button-container-container">
+                    <div class="carousel-select-button-container">
+                        <button title="Select dual gamemode">Select</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div class="gamemode-selection-close-container">
