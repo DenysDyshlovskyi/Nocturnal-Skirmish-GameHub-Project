504f7cc7d222632efe1231c9cb81a9b3e37288fc
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2024-12-13 09:41:40 +0100

    added shop navigation tutorial

--- website/spa/tutorial/navigating_shop.php
+++ website/spa/tutorial/navigating_shop.php
@@ -10,4 +10,10 @@
         The shop is a place where you can buy borders, characters, cards and open crates.
         The shop can be accessed by going on the main hub and clicking the <b>"Shop"</b> button.
     </p>
+    <div>
+        <img src="./img/tutorial/navigating_shop.gif" alt="">
+    </div>
+    <p>
+        The shop is still a work in progress, so for now, none of the functions described over works.
+    </p>
 </article>
